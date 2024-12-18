d1a12c41b7b1e7d0f485fa32c9ea7a5b72b8691e
LiamNelson26 <lnelson490@boyslatin.net>
2024-12-18 14:57:58 +0000

    Note Taking/ Async Dec 13

--- Unit3_python/asyncActivityDec13.py
+++ Unit3_python/asyncActivityDec13.py
@@ -7,5 +7,37 @@ def integerFunction (positive,number):
         print(str(positive)+negative)
 
 
+def IntegerDetection(x):
+    if x > 0:
+        print('this is positive.')
+    elif x == 0:
+        print('this is a zero.')
+    else:
+        print('this number is negative.')
+IntegerDetection(-7)
 
+def movieTicketPriceByAge()
+
+
+def discountFunction(membership, itemPrice): 
+    if membership == 'superShopper':
+        print(' you are getting 10 percent off.')
+        discount= ItemPrice * .1
+        total= itemPrice - discount
+        print(total)
+
+    elif membership== 'megaShopper':
+        print(' you are getting 15 percent off.')
+        discount= ItemPrice * .15
+        total= itemPrice - discount
+        print(total)
+    elif membership== 'ultraShopper':
+        print(' you are getting 20 percent off.')
+        discount= ItemPrice * .2
+        total= itemPrice - discount
+        print(total)
+    else: 
+        print('Error: sorry, that membership  type doesnt exist.')
+
+discountFunction('superShopper', 269)
     
