c231ed6b615d0c76ce429ed891ceda1a382b67b6
LiamNelson26 <lnelson490@boyslatin.net>
2024-12-18 14:57:49 +0000

    Note Taking/ Async Dec 13

---
+++ Unit3_python/asyncActivityDec13.py
@@ -0,0 +1,11 @@
+def integerFunction (positive,number):
+    if number == '10':
+        print('this is a positive number')
+        print(str(positive)+negative)
+    elif number == '-12':
+        print('this is a negative number')
+        print(str(positive)+negative)
+
+
+
+    
