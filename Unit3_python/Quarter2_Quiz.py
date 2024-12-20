5552c2148718be3c7fd53b5f95bf56c6ecb02ff4
LiamNelson26 <lnelson490@boyslatin.net>
2024-12-20 14:36:14 +0000

    12/20 Quiz

---
+++ Unit3_python/Quarter2_Quiz.py
@@ -0,0 +1,32 @@
+# 1.  A function is a form of code to instruct a computer to perform a task.") 
+
+# 2.  The difference between a function parameter and a function argument is that function 
+# parameters are placeholder of data for the function definition, and function arguments are 
+# when we pass data into a function call of evidence and facts.
+
+# 3.  An if/else conditional statements are used to give the ability to control 
+# data types and decide on what we do with the data.
+
+# 4.  Integer data types are whole numbers and not decimals.
+
+# 5.  Boolean data types are "true or false" values.
+
+# 6.  The comparison operator works best for the function.
+
+# 7.  The assignment operator works best for the function.
+
+# 8. 
+
+def passwordLength(long,short):
+    if password == '15':
+        print('Sorry, this password is too long')
+        print(str(long)+short)
+    elif password == '2':
+        print('Sorry, this password is too short.')
+        print(str(long)+short)
+
+
+passwordLength(long,short)
+
+# 9.
+
--- Unit3_python/tempCodeRunnerFile.py
+++ Unit3_python/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-event
+def foodExpiration(month, date, year):
+    expirationMonth: 12 
+    expirationDate: 5
+    expirationYear: 2026
+    if date > expirationDate and year > expirationYear:
+        print('throw food away.')
+    else:
+        print('food is still good.')
+
+foodExpiration(12, 4, 2024)
